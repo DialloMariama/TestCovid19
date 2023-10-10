3cfeb2bf97ababef4ae8ad78d75ee96cd962904a
Mariama Diallo <maridialloisidk@gmail.com>
2023-10-10 10:46:47 +0000

    gestion des erreurs et formulaire

--- index.php
+++ index.php
@@ -19,9 +19,17 @@ $score = 0;
 $resultats_utilisateur = array();
 $erreurs = array();
 
-function validerNomPrenom($nom, $prenom) {
-    if ((strlen($nom) >= 2 && strlen($nom) <= 40) && (strlen($prenom) >= 3 && strlen($prenom) <= 50)) {
-        return preg_match("/^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/", $nom) && preg_match("/^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/", $prenom);
+function validerNom($nom) {
+    if (strlen($nom) >= 2 && strlen($nom) <= 40) {
+        return preg_match("/^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/", $nom);
+    } else {
+        return false;
+    }
+}
+
+function validerPrenom($prenom) {
+    if (strlen($prenom) >= 3 && strlen($prenom) <= 50) {
+        preg_match("/^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/", $prenom);
     } else {
         return false;
     }
@@ -29,13 +37,18 @@ function validerNomPrenom($nom, $prenom) {
 
 
 
-function validerTemperaturePoids($temperature, $poids) {
-    return $temperature >= 30 && $temperature <= 45 && $poids >= 2 && $poids <= 300;
+function validerTemperature($temperature) {
+    return $temperature >= 30 && $temperature <= 45;
+}
+function validerPoids($poids) {
+    return $poids >= 2 && $poids <= 300;
 }
 
 function validerChampsRadio($maux_tete, $diarrhee, $perte_odorat) {
     return !empty($maux_tete) && !empty($diarrhee) && !empty($perte_odorat);
 }
+
+
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nom = isset($_POST['nom']) ? $_POST['nom'] : "";
@@ -47,16 +60,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $age = isset($_POST['age']) ? $_POST['age'] : "";
     $perte_odorat = isset($_POST['perte_odorat']) ? $_POST['perte_odorat'] : "";
 
-    if (!validerNomPrenom($nom, $prenom)) {
-        array_push($erreurs, "Entrez un nom et un prénom valides.");
+    if (!validerNom($nom)) {
+        array_push($erreurs, "Entrez un nom valide.");
     }
 
-    if (!validerTemperaturePoids($temperature, $poids)) {
-        array_push($erreurs, "Entrez des valeurs valides pour la température et le poids.");
+    if (!validerPrenom($prenom)) {
+        array_push($erreurs, "Entrez un prénom valide.");
     }
 
+    if (!validerTemperature($temperature)) {
+        array_push($erreurs, "Entrez une valeur valide pour la température.");
+    }
+
+    if (!validerPoids($poids)) {
+        array_push($erreurs, "Entrez une valeur valide pour le poids.");
+    }
     if (!validerChampsRadio($maux_tete, $diarrhee, $perte_odorat)) {
-        array_push($erreurs, "Veuillez répondre à toutes les questions.");
+        array_push($erreurs, "Les champs radios sont obligatoires.");
     } else {
         switch ($maux_tete) {
             case 'oui':
@@ -159,40 +179,44 @@ echo '<input type="submit" onclick="window.location.href = \'soumettre.php\';" v
         <input type="text" id="nom" name="nom" required pattern="[A-Za-z]+" title="Entrez un nom valide (lettres uniquement)" value="<?php echo $nom; ?> ">
         
         <label for="prenom">Prénom</label>
-        <input type="text" id="prenom" name="prenom" required pattern="[A-Za-z]+" title="Entrez un prenom valide (lettres uniquement)" >
+        <input type="text" id="prenom" name="prenom"  value="<?php echo $prenom; ?> ">
         
         <label for="temperature">Température (°C)</label>
-        <input type="number" id="temperature" name="temperature" step="0.01" min="30" max="45" required>
+        <input type="number" id="temperature" name="temperature" step="0.01" value="<?php echo $temperature; ?>" min="30" max="45" required >
  
         <label for="poids">Poids (kg)</label>
-        <input type="number" id="poids" name="poids" step="0.01" min="2" max="300" required>
+        <input type="number" id="poids" name="poids" step="0.01" value="<?php echo $poids; ?>" min="2" max="300" required >
         
         <label for="maux_tete">Avez-vous des maux de tête ?</label>
-        <input type="radio" id="maux_tete_oui" name="maux_tete" value="oui" required>
+        <input type="radio" id="maux_tete_oui" name="maux_tete" value="oui" <?php if ($maux_tete === 'oui') echo 'checked'; ?> required >
         <label for="maux_tete_oui">Oui</label>
-        <input type="radio" id="maux_tete_non" name="maux_tete" value="non" required>
+        <input type="radio" id="maux_tete_non" name="maux_tete" value="non" <?php if ($maux_tete === 'non') echo 'checked'; ?> required >
         <label for="maux_tete_non">Non</label>
         
         <label for="diarrhee">Avez-vous la diarrhée</label>
-        <input type="radio" id="diarrhee_oui" name="diarrhee" value="oui" required>
+        <input type="radio" id="diarrhee_oui" name="diarrhee" value="oui" <?php if ($diarrhee === 'oui') echo 'checked'; ?> required >
         <label for="diarrhee_oui">Oui</label>
-        <input type="radio" id="diarrhee_non" name="diarrhee" value="non" required>
+        <input type="radio" id="diarrhee_non" name="diarrhee" value="non" <?php if ($diarrhee === 'non') echo 'checked'; ?> required >
         <label for="diarrhee_non">Non</label>
 
         <label for="perte_odorat">Avez-vous une perte d'odeur</label>
-        <input type="radio" id="perte_odorat_oui" name="perte_odorat" value="oui" required>
+        <input type="radio" id="perte_odorat_oui" name="perte_odorat" value="oui"  <?php if ($perte_odorat === 'oui') echo 'checked'; ?> required>
         <label for="perte_odorat_oui">Oui</label>
-        <input type="radio" id="perte_odorat_non" name="perte_odorat" value="non" required>
+        <input type="radio" id="perte_odorat_non" name="perte_odorat" value="non" <?php if ($perte_odorat === 'non') echo 'checked'; ?> required >
         <label for="perte_odorat_non">Non</label>
-    
+            
         <label for="age">Âge</label>
-        <input type="radio" id="age_0_14" name="age" value="0-14" required>
+        <input type="radio" id="age_0_14" name="age" value="0-14" <?php if (isset($age) && $age === '0-14') echo 'checked'; ?> required >
         <label for="age_0_14">0-14 ans</label>
-        <input type="radio" id="age_15_40" name="age" value="15-40" required>
+
+        <input type="radio" id="age_15_40" name="age" value="15-40" <?php if (isset($age) && $age === '15-40') echo 'checked'; ?> required >
         <label for="age_15_40">15-40 ans</label>
-        <input type="radio" id="age_41_100" name="age" value="41-100" required>
+
+        <input type="radio" id="age_41_100" name="age" value="41-100" <?php if (isset($age) && $age === '41-100') echo 'checked'; ?> required >
         <label for="age_41_100">41-100 ans</label>
+
     
+        
         <input type="submit" value="Soumettre">
        
     </form>
